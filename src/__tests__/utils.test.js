ac003f91b229881864480dee4b819cd1d69e231a
+ <+>
2024-03-24 06:19:51 -0700

    complete lab

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,29 @@
-// Your tests here
+import { isPalindrome } from "../utils";
+
+describe("isPalindrome", () => {
+  it("checks whether a given word is a palindrome", () => {
+    const word = 'racecar'
+    expect(isPalindrome(word)).toBe(true)
+  });
+
+  it("checks whether a given word is a palindrome", () => {
+    const word = 'car'
+    expect(isPalindrome(word)).toBe(false)
+  });
+
+  it("checks whether a given word is a palindrome", () => {
+    const word = ''
+    expect(isPalindrome(word)).toBe(false)
+  });
+
+  it('throws on invalid characters', () => {
+    const word = '$#9glib2=_-?-_=2bilg9#$'
+    expect(() => isPalindrome(word)).toThrow();
+  });
+
+  it('throws on non string arguments', () => {
+    const word = 42
+    expect(() => isPalindrome(word)).toThrow();
+  });
+});
+
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,13 @@
-// Your code here
+export function isPalindrome(word){
+    if(!word)
+        return false
+
+    if(!/^[A-Za-z]+$/.test(word))
+        throw new Error('Argument is not a word!');
+    else if(typeof word !== 'string')
+        throw new Error('Argument is not a string!')
+
+    const loweredWord = word.toLowerCase()
+    const reverseWord = loweredWord.split('').reverse().join('')
+    return loweredWord === reverseWord
+}
